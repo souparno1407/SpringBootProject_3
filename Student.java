0f885d0903a76bb18fa46b6d2053b6158e6d965b
shreya99oak <shreya99oak@gmail.com>
2021-08-05 16:09:24 +0530

    POJO

---
+++ Student.java
@@ -0,0 +1,35 @@
+
+public class Student {
+	
+	String name;
+	String address;
+	int studId;
+
+	public Student(String name, String address, int studId) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.studId = studId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getStudId() {
+		return studId;
+	}
+	public void setStudId(int studId) {
+		this.studId = studId;
+	}
+	
+	
+}
